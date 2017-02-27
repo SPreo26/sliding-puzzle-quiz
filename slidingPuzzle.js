c8b904d8f448be66f5429dcc3f95646ec6eb2c1c
SPreo26 <SPreo26@users.noreply.github.com>
2017-02-27 01:43:17 -0800

    fixed js code and tile sliding is now fully functional

--- slidingPuzzle.js
+++ slidingPuzzle.js
@@ -4,9 +4,9 @@ $(document).ready(function(){
     document.body.onclick = function(event){
         elem = window.event? event.srcElement: event.target;
         if(elem.className && elem.className.indexOf('tile') != -1){
-            var offset = window.slidingPuzzle.getSlideOffset(elem);
-            if (offset){
-                window.slidingPuzzle.moveElement(elem,offset);
+            var slideOffset = window.slidingPuzzle.getSlideOffset(elem);
+            if (slideOffset){
+                window.slidingPuzzle.moveElement(elem,slideOffset);
             }
         }
     }
@@ -15,10 +15,10 @@ $(document).ready(function(){
 
     window.slidingPuzzle.getSlideOffset = 
         function(elem){
-            var slideOffset;
+            var slideOffset = {};
             if (elem){
                 var x = elem.offsetLeft;
-                var y = elem.offsetRight;
+                var y = elem.offsetTop;
                 var rightNeighbor = document.elementFromPoint(x+window.slidingPuzzle.tileTotalWidth, y);
                 var bottomNeighbor = document.elementFromPoint(x, y+window.slidingPuzzle.tileTotalWidth);
                 var leftNeighbor = document.elementFromPoint(x-window.slidingPuzzle.tileTotalWidth, y);
@@ -29,44 +29,43 @@ $(document).ready(function(){
                 var southWestNeighbor = document.elementFromPoint(x-window.slidingPuzzle.tileTotalWidth, y+window.slidingPuzzle.tileTotalWidth);
                 var northWestNeighbor = document.elementFromPoint(x-window.slidingPuzzle.tileTotalWidth, y-window.slidingPuzzle.tileTotalWidth);
 
-                if (rightNeighbor.className == 'sliding-puzzle' && 
-                    (northEastNeighbor.className == 'tile' || southEastNeighbor.parentElement.className == 'tile')
+                if (rightNeighbor && rightNeighbor.className == 'sliding-puzzle' && 
+                    ((northEastNeighbor && northEastNeighbor.className == 'tile') || (southEastNeighbor && southEastNeighbor.className == 'tile'))
                     //a right neighbor can only be considered the empty square if either the northeast or southeast neighbor is a tile (otherwise the right neighbor of a tile could potentially land on the right border of the puzzle which will still return 'sliding-puzzle' as the class name; but this will be a false flag, rather than an actual empty square)
                 ){
                     slideOffset.x = window.slidingPuzzle.tileTotalWidth;
                     slideOffset.y = 0;
                 }
 
-                else if (bottomNeighbor.className == 'slidingPuzzle' && 
-                    (southEastNeighbor.className == 'tile' || southWestNeighbor.parentElement.className == 'tile')
+                else if (bottomNeighbor && bottomNeighbor.className == 'sliding-puzzle' && 
+                    ((southEastNeighbor && southEastNeighbor.className == 'tile') || (southWestNeighbor && southWestNeighbor.className == 'tile'))
                 ){
                     slideOffset.x = 0;
                     slideOffset.y = window.slidingPuzzle.tileTotalWidth;
                 }
 
-                else if (leftNeighbor.className == 'slidingPuzzle' && 
-                    (southWestNeighbor.className == 'tile' || northWestNeighbor.parentElement.className == 'tile')
+                else if (leftNeighbor && leftNeighbor.className == 'sliding-puzzle' && 
+                    ((southWestNeighbor && southWestNeighbor.className == 'tile') || (northWestNeighbor && northWestNeighbor.className == 'tile'))
                     //a diagonal neighbor check may not be necessary for testing if a left neighbor (or top neighbor) is an empty square; however it is still done to be consistent and to be safe in case the padding+border of the puzzle is ever changed to be >= the width of a tile - then a false flag could still happen in this case
                 ){
                     slideOffset.x = -window.slidingPuzzle.tileTotalWidth;
                     slideOffset.y = 0;
                 }
 
-                else if (topNeighbor.className == 'slidingPuzzle' && 
-                    (northWestNeighbor.className == 'tile' || northEastNeighbor.parentElement.className == 'tile')
+                else if (topNeighbor && topNeighbor.className == 'sliding-puzzle' && 
+                    ((northWestNeighbor && northWestNeighbor.className == 'tile') || (northEastNeighbor && northEastNeighbor.className == 'tile'))
                 ){
                     slideOffset.x = 0;
                     slideOffset.y = -window.slidingPuzzle.tileTotalWidth;
                 }
             }
-            console.log(slideOffset);
             return slideOffset;
         }
 
     window.slidingPuzzle.moveElement = 
         function(elem,offset){
-            elem.style.left = offset.x+'px';
-            elem.style.top = offset.y+'px';
+            elem.style.left = (parseInt(elem.style.left||0)+offset.x)+'px';
+            elem.style.top = (parseInt(elem.style.top||0)+offset.y)+'px';
         }
 
 });
